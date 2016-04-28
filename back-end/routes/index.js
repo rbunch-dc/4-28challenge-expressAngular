c18494bff19b17db5dd3a34d27a7ae4b896d4fd7
Robert Bunch <rob@digitalcrafts.com>
2016-04-28 13:33:37 -0400

    Moved to mongoose

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -8,17 +8,21 @@ var Student = require('../models/students');
 /* GET home page. */
 router.get('/students/:sortMethod', function(req, res, next) {
 
+	students = [];
 	Student.find({}, function(error, document){
-		console.log(document);
+		for(i=0; i<document.length; i++){
+			students.push(document[i].name);
+		}
+		if(req.params.sortMethod == "sort"){
+			students.sort()	
+		}else if(req.params.sortMethod == "reverse"){
+			students.sort()	
+		  	students.reverse();
+	  	}
+		res.json(students);
 	});
 
-	if(req.params.sortMethod == "sort"){
-		students.sort()	
-	}else if(req.params.sortMethod == "reverse"){
-		students.sort()	
-	  	students.reverse();
-  	}
-  	res.json(students);
+ 
 });
 
 module.exports = router;
